31d47a6e5f2c2f5334b64b2831100afb385ad0ee
Raul Luis <soyrauluis@gmail.com>
2022-06-28 00:05:49 -0400

    Add db connection

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
-using Microsoft.EntityFrameworkCore
-using coinsapi.Models
+using coinsapi.Data;
+using coinsapi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Console;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Environment.IsDevelopment() ? builder.Configuration.GetConnectionString("DefaultConnection") : Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-// UriBuilder.Services.AddDbContext<CoinsDbContext>(opt => opt.UseCosmos())
+// using var loggerFactory = LoggerFactory.Create(builder =>
+// {
+//     builder.AddSimpleConsole(i => i.ColorBehavior = LoggerColorBehavior.Disabled);
+// });
+
+// var logger = loggerFactory.CreateLogger<Program>();
+
+builder.Services.AddDbContext<CoinsDbContext>(opt => opt.UseNpgsql(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +29,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
